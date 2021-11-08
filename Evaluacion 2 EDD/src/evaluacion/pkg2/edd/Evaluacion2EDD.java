f7ba113849b574d112667eec77d0aded682adda9
AlejoDVZ <93200646+AlejoDVZ@users.noreply.github.com>
2021-11-08 18:10:09 -0400

    creacion del archivo

---
+++ Evaluacion 2 EDD/src/evaluacion/pkg2/edd/Evaluacion2EDD.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evaluacion.pkg2.edd;
+
+/**
+ *
+ * @author Alejo
+ */
+public class Evaluacion2EDD {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
